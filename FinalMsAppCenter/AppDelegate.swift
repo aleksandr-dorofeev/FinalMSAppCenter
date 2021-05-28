bf9244683034f9ebf44a7118c83dbd3c3a7d237d
Aleksanr Dorofeev <aleksandr.dorofeev@akvelon.com>
2021-05-28 12:27:58 +0300

    First commit

---
+++ FinalMsAppCenter/AppDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  AppDelegate.swift
+//  FinalMsAppCenter
+//
+//  Created by Akvelon on 5/28/21.
+//
+
+import UIKit
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
+import AppCenterDistribute
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        AppCenter.start(withAppSecret: "fc9e7cc3-1f19-4df0-8960-be3ecda6431a", services:[
+            Distribute.self,
+            Analytics.self,
+            Crashes.self])
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ FinalMsAppCenter/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  FinalMsAppCenter
+//
+//  Created by Akvelon on 5/28/21.
+//
+
+import UIKit
+import AppCenter
+import AppCenterCrashes
+import AppCenterAnalytics
+
+class ViewController: UIViewController {
+    // Buttons
+    let crashButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Crash", for: .normal)
+        button.addTarget(self, action: #selector(buttonIsCrash) , for: .touchUpInside)
+        button.backgroundColor = .red
+        return button
+    }()
+    
+    let clickButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Click", for: .normal)
+        button.addTarget(self, action: #selector(buttonIsClick) , for: .touchUpInside)
+        button.backgroundColor = .blue
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        AppCenter.start(withAppSecret: "fc9e7cc3-1f19-4df0-8960-be3ecda6431a", services:[
+          Analytics.self,
+          Crashes.self
+        ])
+        
+        view.addSubview(crashButton)
+        view.addSubview(clickButton)
+        
+        createClickButtonConstrait()
+        createCrashTheAppConstrait()
+    }
+    
+    // Actions
+    @objc func buttonIsClick() {
+        Analytics.trackEvent("Change button color")
+        if clickButton.backgroundColor == .blue {
+            clickButton.backgroundColor = .green
+        } else {
+            clickButton.backgroundColor = .blue
+        }
+    }
+    
+    @objc func buttonIsCrash() {
+        fatalError()
+    }
+}
+
+extension ViewController {
+    
+    // Constraits
+    private func createClickButtonConstrait() {
+        clickButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        clickButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/3).isActive = true
+        clickButton.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        clickButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    private func createCrashTheAppConstrait() {
+        crashButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        crashButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/3).isActive = true
+        crashButton.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        crashButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+}
+
